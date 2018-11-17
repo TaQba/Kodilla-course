8c4d70a402af8808227da1e37f084a20008ddd3c
TaQba <jakub.tadych@gmail.com>
2018-11-17 18:04:24 +0000

    COurse 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,41 +1,18 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.*;
-import com.kodilla.stream.reference.*;
-import com.kodilla.stream.beautifier.*;
-import com.kodilla.stream.iterate.*;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-        Processor processor = new Processor();
-        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
-        processor.execute(executeSaySomething);
-        Executor codeToExecute = () -> System.out.println("This is an example text.");
-        processor.execute(codeToExecute);
-        processor.execute(() -> System.out.println("This is an example text."));
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));
 
-
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-
-        System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-
-        System.out.println("TASK 7.1");
-        PoemBeautifier poem = new PoemBeautifier();
-        poem.beautify("Impsum Lorem", String::toUpperCase);
-        poem.beautify("Impsum Lorem", String::toLowerCase);
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
-
+        System.out.println(theResultStringOfBooks);
     }
 }
