cbbfb9619d42a6bd16d15b09c55cbe9d17379cf2
TaQba <jakub.tadych@gmail.com>
2018-11-20 19:11:13 +0000

    NIestety plik zostal dodany przy poprzedmin zadaniu teraz dodaje tylko zmiany aby zaflagowac pliki

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -6,10 +6,13 @@ import org.junit.*;
 public class ArrayOperationsTestSuite {
     @Test
     public void testGetAverage () {
+        //Given
         int[] table = new int[20];
+        //WHEN
         for (int i = 0; i < 20; i++)
             table[i] = i + 1;
 
+        //Then
         Assert.assertEquals(10 , ArrayOperations.getAverage(table),0.5);
     }
 }
