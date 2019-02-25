2745bc4492de1b8c7278b9655de31f45191cd34b
jakub.tadych@gmail.com <!Madzia1980>
2019-02-25 22:04:27 +0000

    course 20,1

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderDto.java
@@ -0,0 +1,21 @@
+package com.kodilla.patterns2.facade.api;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDto {
+    private final List<ItemDto> items = new ArrayList();
+
+    public List<ItemDto> OrderDto() {
+        return items;
+    }
+
+    public void addItem(final ItemDto item) {
+        items.add(item);
+    }
+
+    public List<ItemDto> getItems() {
+        return items;
+    }
+}
