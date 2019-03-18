9b783736faed43b104fa456f21be335ed6fcc7a7
jakub.tadych@gmail.com <!Madzia1980>
2019-03-18 21:49:32 +0000

    22.3

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -1,4 +1,35 @@
 package com.kodilla.jdbc;
 
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertEquals;
+
 public class StoredProcTestSuite {
+    @Test
+    public void testUpdateVipLevels() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sql = "UPDATE readers SET VIP_LEVEL=\"Not set\"";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sql);
+
+        //When
+        String sqlProc = "CALL UpdateVipLevels()";
+        statement.executeUpdate(sqlProc);
+
+        //Then
+        String sqlCheck = "SELECT COUNT(*) AS HOW_MANY FROM readers WHERE VIP_LEVEL=\"Not set\"";
+        ResultSet rs = statement.executeQuery(sqlCheck);
+        int howMany = -1;
+        if(rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertEquals(0,howMany);
+    }
+
 }
